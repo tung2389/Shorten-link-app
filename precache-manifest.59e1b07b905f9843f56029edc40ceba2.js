c145fc8869b247302be34ad01cc39944a678ed98
tung2389 <luukhanhtung2002@gmail.com>
2019-03-15 13:59:15 +0700

    upload to gh-pages

---
+++ precache-manifest.59e1b07b905f9843f56029edc40ceba2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "9e293e3679d532d5bdd2",
+    "url": "./static/js/main.27c3f702.chunk.js"
+  },
+  {
+    "revision": "d1a2e7273be037d152b6",
+    "url": "./static/js/2.6971ebbd.chunk.js"
+  },
+  {
+    "revision": "9e293e3679d532d5bdd2",
+    "url": "./static/css/main.19485a28.chunk.css"
+  },
+  {
+    "revision": "32b890badf20ca25c6b1e6aa0c7e65eb",
+    "url": "./index.html"
+  }
+];
